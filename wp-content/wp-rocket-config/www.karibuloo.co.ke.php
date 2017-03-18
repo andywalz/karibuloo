b9d4b0e3aaf605c1c37b252ec2b64766ec11b85e
Andy Walz <andywalz@gmail.com>
2017-03-17 22:06:20 -0500

    Plugins sync

---
+++ wp-content/wp-rocket-config/www.karibuloo.co.ke.php
@@ -0,0 +1,17 @@
+<?php
+defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
+
+$rocket_cookie_hash = '5cf68430c7d97b812ddaca96f4c793a6';
+$rocket_secret_cache_key = '58cc43033be22474835029';
+$rocket_cache_mobile = '0';
+$rocket_do_caching_mobile_files = '0';
+$rocket_common_cache_logged_users = '0';
+$rocket_cache_ssl = '0';
+$rocket_cache_reject_uri = '.*/feed/?|/wp-json/(.*)';
+$rocket_cache_reject_cookies = 'wordpress_logged_in_|wp-postpass_|wptouch_switch_toggle|comment_author_|comment_author_email_';
+$rocket_cache_reject_ua = 'facebookexternalhit';
+$rocket_cache_query_strings = array (
+);
+$rocket_cache_mandatory_cookies = '';
+$rocket_cache_dynamic_cookies = array (
+);
